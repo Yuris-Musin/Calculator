489adda63cf0bbbb07be6150e62de93eff0eb987
Yuris <yuris.musin@mail.ru>
2026-04-08 12:47:22 +0500

    Добавил цвета для тем

--- app/src/main/java/com/musin/calculate/ui/theme/Color.kt
+++ app/src/main/java/com/musin/calculate/ui/theme/Color.kt
@@ -2,10 +2,19 @@ package com.musin.calculate.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val White = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Black = Color(0xFF0D0D0D)
+
+val Grey100 = Color(0xCCFFFFFF)
+val Grey200 = Color(0xFFE6E7EB)
+val Grey300 = Color(0xFFD9D9D9)
+val Grey400 = Color(0x802C2E33)
+val Grey500 = Color(0xFF333333)
+val Grey600 = Color(0xE50D0D0D)
+
+val Green200 = Color(0xCC006600)
+val Green100 = Color(0x80006600)
+
+val Blue100 = Color(0x80084276)
+val Blue200 = Color(0xCC084276)
--- app/src/main/java/com/musin/calculate/ui/theme/Theme.kt
+++ app/src/main/java/com/musin/calculate/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.musin.calculate.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,32 +11,33 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    background = Black,
+    onBackground = White,
+    primary = Grey400,
+    secondary = Green200,
+    tertiary = Blue200,
+    primaryContainer = Grey500,
+    onPrimaryContainer = White,
+    onSecondaryContainer = Grey100
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    background = White,
+    onBackground = Grey600,
+    primary = Grey200,
+    secondary = Green100,
+    tertiary = Blue100,
+    primaryContainer = Grey300,
+    onPrimaryContainer = Black,
+    onSecondaryContainer = Black
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun CalculateTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
