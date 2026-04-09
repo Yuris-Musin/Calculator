8aef49bbbbe33c86f08676a43357ce6cfb098a93
Yuris <yuris.musin@mail.ru>
2026-04-09 20:39:00 +0500

    Добавили библиотеку mXparser

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.mathparser.org.mxparser)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
