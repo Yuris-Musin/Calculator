5430243884aeafa6b69869faa8ec8bda1cbb35c3
Yuris <yuris.musin@mail.ru>
2026-04-09 10:53:16 +0500

    Добавил state рекомпозиции

--- app/src/main/java/com/musin/calculate/Calculator.kt
+++ app/src/main/java/com/musin/calculate/Calculator.kt
@@ -1,6 +1,8 @@
 package com.musin.calculate
 
+import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,15 +16,18 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+var expression = mutableStateOf("45x8")
+var result = mutableStateOf("360")
 
 @Composable
 fun Calculator(
@@ -49,13 +54,13 @@ fun Calculator(
             horizontalAlignment = Alignment.End
         ) {
             Text(
-                text = "45x8",
+                text = expression.value,
                 fontSize = 36.sp,
                 fontWeight = FontWeight.SemiBold,
                 color = MaterialTheme.colorScheme.onPrimaryContainer
             )
             Text(
-                text = "360",
+                text = result.value,
                 fontSize = 17.sp,
                 fontWeight = FontWeight.SemiBold,
                 color = MaterialTheme.colorScheme.onSecondaryContainer
@@ -107,6 +112,11 @@ fun Calculator(
             Box(
                 modifier = Modifier.weight(1f)
                     .clip(CircleShape)
+                    .clickable {
+                        Log.d("Calculator", "AC click")
+                        expression.value = ""
+                        result.value = ""
+                    }
                     .background(MaterialTheme.colorScheme.secondary)
                     .aspectRatio(1f),
                 contentAlignment = Alignment.Center
